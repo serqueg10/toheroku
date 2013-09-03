81639ebaa0cee8b0fb3e450e0eaaee9ed4566004
Serqueg10 <sergiogr9506@gmail.com>
2013-09-03 10:22:07 -0500

    first commit porque quiero

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toheroku::Application.config.secret_key_base = 'dc44ef1e152c86e2507fd54986bf2dd8db8784a3e85c5693c93d3ece8c6d7ff53cfecfa4680945659093aec0d7ee18b018d5414281f1e950692f2ccc5c84c21d'
